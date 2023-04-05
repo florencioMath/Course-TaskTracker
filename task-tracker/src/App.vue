5f7720c59201d9b757b9ddedc763e2ec04b3aedb
Matheus Florêncio <68713424+florencioMath@users.noreply.github.com>
2023-04-05 13:50:37 -0300

    feat: Emit events form Task to Tasks to App.

--- task-tracker/src/App.vue
+++ task-tracker/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <Header title="Task Tracker" />
-    <Tasks :tasks="tasks" />
+    <Tasks @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
@@ -16,6 +16,15 @@ export default {
     return {
       tasks: [],
     };
+  },
+  methods: {
+    deleteTask({ text, id }) {
+      if (confirm(`Você quer deletar a task: ${text}`)) {
+        this.tasks = this.tasks.filter((task) => {
+          return task.id !== id;
+        });
+      }
+    },
   },
   created() {
     this.tasks = [
